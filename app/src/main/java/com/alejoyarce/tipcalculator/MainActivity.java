6547c4033c4706191669fd0d90e0b50f99f5d37a
Alejo Yarce <yarce@kometsales.com>
2016-06-01 18:22:44 -0500

    ButterKnife

--- app/src/main/java/com/alejoyarce/tipcalculator/MainActivity.java
+++ app/src/main/java/com/alejoyarce/tipcalculator/MainActivity.java
@@ -1,18 +1,45 @@
 package com.alejoyarce.tipcalculator;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.inputBill)
+    private EditText inputBill;
+    @Bind(R.id.btnCalculate)
+    private Button btnCalculate;
+    @Bind(R.id.inputPercentage)
+    private EditText inputPercentage;
+    @Bind(R.id.btnIncrease)
+    private Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    private Button btnDecrease;
+    @Bind(R.id.btnClear)
+    private Button btnClear;
+    @Bind(R.id.textTip)
+    private EditText textTip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 
     @Override
@@ -27,6 +54,22 @@ public class MainActivity extends AppCompatActivity {
              about();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.btnCalculate)
+    public void calculateTip() {
+        hideKeyBoard();
+
+    }
+
+    private void hideKeyBoard() {
+        InputMethodManager inputMethodManager = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        try {
+            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), inputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (NullPointerException npe) {
+            Log.e(getLocalClassName() , Log.getStackTraceString(npe));
+        }
     }
 
     private void about() {
--- app/src/main/java/com/alejoyarce/tipcalculator/TipCalcApp.java
+++ app/src/main/java/com/alejoyarce/tipcalculator/TipCalcApp.java
@@ -5,6 +5,8 @@ import android.app.Application;
 public class TipCalcApp extends Application {
 
     private final static String ABOUT_URL = "https://github.com/AlejoYarce/TipCalculator";
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_PERCENTAGE = 10;
 
     public String getAboutUrl() {
         return ABOUT_URL;
